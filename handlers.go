7d9bd55ac84dc9dda57943e6365e39d68377a5bf
Paweł Piekut <pawelpiekut212@gmail.com>
2025-12-03 19:51:52 +0100

    add services and handler

---
+++ handlers.go
@@ -0,0 +1,89 @@
+package main
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/gofiber/fiber/v2"
+	"github.com/pablotartak/parser-ofert/parser"
+	"strings"
+)
+func main() {
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendFile("index.html")
+	})
+
+	app.Post("/parser", func(c *fiber.Ctx) error {
+		raw := string(c.Body())
+		if raw == "" {
+			return fmt.Errorf("Brak danych w body POST")
+		}
+
+		lines := strings.Split(raw, "\n")
+
+		var paramLines []string
+		var mediaLine, infoLine string
+		var opisLines []string
+		mode := "parametry"
+//przełączanie sekcji według nagłówków
+		for _, line := range lines {
+			line = strings.TrimSpace(line)
+			if line == "" {
+				continue
+			}
+
+			switch line {
+			case "Media":
+				mode = "media"
+				continue
+			case "Informacje dodatkowe":
+				mode = "info"
+				continue
+			case "Opis":
+				mode = "opis"
+				continue
+			}
+
+			switch mode {
+			case "parametry":
+				paramLines = append(paramLines, line)
+			case "media":
+				if mediaLine != "" {
+					mediaLine += " "
+				}
+				mediaLine += line
+			case "info":
+				if infoLine != "" {
+					infoLine += " "
+				}
+				infoLine += line
+			case "opis":
+				opisLines = append(opisLines, line)
+			}
+		}
+// Wywołanie parserów dla każdej sekcji
+		parametry := parseParametry(paramLines)
+		media := parseMedia(mediaLine)
+		info := parseInfo(infoLine)
+		opis, endParams := parseOpis(opisLines)
+
+		final := map[string]interface{}{
+			"Parametry oferty": parametry,
+			"Media": media,
+			"Informacje dodatkowe": info,
+			"Opis": opis,
+		}
+		for k, v := range endParams {
+			final[k] = v
+		}
+
+		jsonData, err := json.MarshalIndent(final, "", "  ")
+		if err != nil {
+			return c.Status(500).SendString(fmt.Sprintf("Błąd konwersji do JSON: %v", err))
+		}
+
+		return c.SendString(string(jsonData))
+	})
+
+	app.Listen(":8080")
+}
--- parser/service.go
+++ parser/service.go
@@ -1,9 +1,6 @@
-package main
+package services
 
 import (
-	"encoding/json"
-	"fmt"
-	"github.com/gofiber/fiber/v2"
 	"strings"
 )
 //Funkcja dostaje liste linijek pocietych w main i dzieli je na key i value
@@ -84,86 +81,4 @@ func parseOpis(lines []string) (string, map[string]string) {
 	}
 
 	return opisBuilder.String(), endParams
-}
-
-func main() {
-	app := fiber.New()
-
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendFile("index.html")
-	})
-
-	app.Post("/parser", func(c *fiber.Ctx) error {
-		raw := string(c.Body())
-		if raw == "" {
-			return fmt.Errorf("Brak danych w body POST")
-		}
-
-		lines := strings.Split(raw, "\n")
-
-		var paramLines []string
-		var mediaLine, infoLine string
-		var opisLines []string
-		mode := "parametry"
-//przełączanie sekcji według nagłówków
-		for _, line := range lines {
-			line = strings.TrimSpace(line)
-			if line == "" {
-				continue
-			}
-
-			switch line {
-			case "Media":
-				mode = "media"
-				continue
-			case "Informacje dodatkowe":
-				mode = "info"
-				continue
-			case "Opis":
-				mode = "opis"
-				continue
-			}
-
-			switch mode {
-			case "parametry":
-				paramLines = append(paramLines, line)
-			case "media":
-				if mediaLine != "" {
-					mediaLine += " "
-				}
-				mediaLine += line
-			case "info":
-				if infoLine != "" {
-					infoLine += " "
-				}
-				infoLine += line
-			case "opis":
-				opisLines = append(opisLines, line)
-			}
-		}
-// Wywołanie parserów dla każdej sekcji
-		parametry := parseParametry(paramLines)
-		media := parseMedia(mediaLine)
-		info := parseInfo(infoLine)
-		opis, endParams := parseOpis(opisLines)
-
-		final := map[string]interface{}{
-			"Parametry oferty": parametry,
-			"Media": media,
-			"Informacje dodatkowe": info,
-			"Opis": opis,
-		}
-		for k, v := range endParams {
-			final[k] = v
-		}
-
-		jsonData, err := json.MarshalIndent(final, "", "  ")
-		if err != nil {
-			return c.Status(500).SendString(fmt.Sprintf("Błąd konwersji do JSON: %v", err))
-		}
-
-		return c.SendString(string(jsonData))
-	})
-
-	app.Listen(":8080")
 }
